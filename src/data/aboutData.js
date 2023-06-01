413fda500861d5fc602be2a5e75280d6dba2043d
Alfat <alfat.cse.21@gmail.com>
2023-06-01 11:49:46 +0200

    profile data edited

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -3,5 +3,5 @@ export const aboutData = {
   description1:
     "My name is Alfat Jahan Rony. I'm a web designer and developer based in Frankfurt, Germany.",
   description2:
-    "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, Express , React, NextJs, Redux, Bootstrap, TailwindCSS, Ant Design, Material UI, MongoDB, Mysql, PostgreSQL, Firebase, Cloudinary, etc.",
+    "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes JavaScript, TypeScript, Nodejs, Express , React, NextJs, Redux, Bootstrap, TailwindCSS, Daisy UI, Ant Design, Material UI, MongoDB, Mysql, PostgreSQL, Jest, Firebase, Cloudinary, etc.",
 };
