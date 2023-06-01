428a2a3e4f2c927ddaf7f383d1395b94c99fa7ea
Alfat <alfat.cse.21@gmail.com>
2023-06-01 11:35:03 +0200

    src

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "My name is Alfat Jahan Rony. I'm a web designer and developer based in Dhaka, Bangladesh.",
+    "My name is Alfat Jahan Rony. I'm a web designer and developer based in Frankfurt, Germany.",
   description2:
-    "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, Nestjs, React, NextJs, Redux, Bootstrap, Ant Design, Material UI, MongoDB, Mysql, PostgreSQL, Firebase, Cloudinary, etc.",
+    "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, Express , React, NextJs, Redux, Bootstrap, TailwindCSS, Ant Design, Material UI, MongoDB, Mysql, PostgreSQL, Firebase, Cloudinary, etc.",
 };
