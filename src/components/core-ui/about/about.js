062d8c66a445cc1bde41fc4ab7c062fd576838a2
Alfat <alfat.cse.21@gmail.com>
2023-06-02 22:23:32 +0200

    [Done] about me section

--- src/components/core-ui/about/about.js
+++ src/components/core-ui/about/about.js
@@ -1,34 +1,61 @@
-import { Container } from '@mui/material';
-import React, { useContext } from 'react';
-import codings from '../../../assets/lottie/coding.json';
-import { ThemeContext } from '../../../contexts/theme-context';
-import { aboutData } from '../../../data/aboutData';
-import AnimationLottie from '../../helper/animation-lottie';
-import './about.css';
+import { Container } from "@mui/material";
+import React, { useContext } from "react";
+import codings from "../../../assets/lottie/coding.json";
+import { ThemeContext } from "../../../contexts/theme-context";
+import { aboutData } from "../../../data/aboutData";
+import AnimationLottie from "../../helper/animation-lottie";
+import "./about.css";
 
 function About() {
-
-    const { theme } = useContext(ThemeContext);
-    return (
-        <div style={{ backgroundColor: theme.secondary }}>
-            <Container className="about" id="about">
-                <div className="line-styling">
-                    <div className="style-circle" style={{ backgroundColor: theme.primary }}></div>
-                    <div className="style-circle" style={{ backgroundColor: theme.primary }}></div>
-                    <div className="style-line" style={{ backgroundColor: theme.primary }}></div>
-                </div>
-                <div className="about-body">
-                    <div className="about-description">
-                        <h2 style={{ color: theme.primary }}>{aboutData.title}</h2>
-                        <p style={{ color: theme.tertiary }}>{aboutData.description1}<br /><br />{aboutData.description2}</p>
-                    </div>
-                    <div className="about-animation">
-                        <AnimationLottie animationPath={codings} />
-                    </div>
-                </div>
-            </Container>
+  const { theme } = useContext(ThemeContext);
+  return (
+    <div style={{ backgroundColor: theme.secondary }}>
+      <Container className="about" id="about">
+        <div className="line-styling">
+          <div
+            className="style-circle"
+            style={{ backgroundColor: theme.primary }}
+          ></div>
+          <div
+            className="style-circle"
+            style={{ backgroundColor: theme.primary }}
+          ></div>
+          <div
+            className="style-line"
+            style={{ backgroundColor: theme.primary }}
+          ></div>
         </div>
-    )
+        <div className="about-body">
+          <div className="about-description">
+            <h2 style={{ color: theme.primary }}>{aboutData.title}</h2>
+            <p style={{ color: theme.tertiary, textAlign: "center" }}>
+              {aboutData.description1}
+              <br />
+
+              {aboutData.description2}
+            </p>
+            <p style={{ textAlign: "center" }}>
+              {aboutData.description3.map((i, index) => (
+                <React.Fragment key={index}>
+                  <strong
+                    style={{
+                      color: index % 2 === 0 ? "DodgerBlue" : "OrangeRed",
+                    }}
+                  >
+                    {i}
+                  </strong>
+                  {index !== aboutData.description3.length - 1 && ", "}
+                </React.Fragment>
+              ))}
+            </p>
+          </div>
+          <div className="about-animation">
+            <AnimationLottie animationPath={codings} />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 }
 
 export default About;
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,13 @@
-
 export const headerData = {
-    name: 'ABU SAID',
-    title: "Hello! I am",
-    desciption: "Hello! I am ABU SAID, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://i.ibb.co/ZmdNH6x/abu-said.jpg',
-    imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
-    resumePdf: 'https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/edit?usp=sharing'
-}
+  name: "Alfat Jahan Rony",
+  title: "Hello! I am",
+  desciption:
+    "Hello! I am Alfat Jahan Rony, a dedicated and passionate software developer with a strong drive to create innovative solutions. With one year of experience in software development, I have honed my skills in.",
+  desciption1:
+    "I thrive in collaborative environments, where I can leverage my problem-solving abilities and attention to detail to craft efficient and elegant solutions.",
+  desciption2: ["JavaScript", "TypeScript", "Node Js", "Express Js"],
+  image: "https://i.ibb.co/ngB3yqM/cv-pic.jpg",
+  imagebw: "https://i.ibb.co/31WYw3p/llmnsnssh-modified.jpg",
+  resumePdf:
+    "https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/edit?usp=sharing",
+};
